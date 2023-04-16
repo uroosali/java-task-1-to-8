22d57ea51e11927946caa30fd0d9b03d2f28b5ec
uroos ali <uroosali1999@gmail.com>
2023-04-16 21:20:48 +0500

    java task

---
+++ index.js
@@ -0,0 +1,132 @@
+//Task 1     
+let zakatPercentage = 0.025;
+let userInput = +prompt("Enter your Zakat value");
+let  Result = zakatPercentage *  userInput;
+alert("Your Zakat value is "+ Result);
+
+           // Task 2
+        //   let Wheat = 250;
+        //   let Dates = 2100;
+        //   let Barley = 450;
+        //   let Raisins = 2800;
+        //   let familyMembers = prompt("Enter the total number of Family Members")
+        //   let fitrahMethod = prompt("Choose a fitrah Method \n 1. Wheat   =  Rs.250 \n 2. Dates   =   Rs.2100 \n 3. Barley   =  Rs.450 \n 4. Raisins  =  Rs.280")
+        //    fitrahMethod = fitrahMethod.toLowerCase()
+        //    if(fitrahMethod == 'wheat')
+        //    {
+        //        let totalFitrah = Wheat*familyMembers
+        //    }
+        //    else if(fitrahMethod == 'dates')
+        //    {
+        //        let totalFitrah = Dates*familyMembers
+        //    }
+        //    else if(fitrahMethod == 'barley')
+        //    {
+        //        let totalFitrah = Barley*familyMembers
+        //    }
+        //    else if(fitrahMethod == 'raisins')
+        //    {
+        //        let totalFitrah = Raisins*familyMembers
+        //    }
+        //    else{
+        //        alert("Invalid Input")
+        //    }
+        //    alert("Total Fitrah Amount of Your Family is: "+totalFitrah)
+           
+           // Task 3
+// let secretNumber = 6;
+// let userInput = prompt("Guess a Secret Number: ");
+// if(userInput == 6)
+// {
+//         alert("Congratulations! You guessed the secret number");
+// }
+// else if(userInput > 6)
+// {
+//         alert("Entered number is too high, Guess Again")
+// }
+// else
+// {
+//         alert("Entered number is too low, Guess Again")
+// }
+
+           // Task 4
+// let userInput = prompt("Enter your name");
+// userName = userInput.toLowerCase();
+//  let a= userName[0].toUpperCase();
+//  let b = a + userName.slice(1);
+//  alert(b);
+
+
+
+           // Task 5
+// let contactNames=[];
+// let contactNumbers=[];
+// contactNumbers.push(prompt("Enter contact number: "));
+
+// contactNames.push(prompt("Enter contact name: "));
+// contactNumbers.push(prompt("Enter contact number: "));
+
+// contactNames.push(prompt("Enter contact name: "));
+// contactNumbers.push(prompt("Enter contact number: "));
+
+// alert("Name: "+contactNames[0]+"  "+"Number: "+contactNumbers[0]+"\nName: "+contactNames[1]+"  "+"Number: "+contactNumbers[1]+"\nName: "+contactNames[2]+"  "+"Number: "+contactNumbers[2]);
+
+       //     Task 6
+// let Products = ['serum' ,'toner' ,'eye cream' ,'cleanser'];
+// let productSell = prompt("Enter the Position of that elemment you want: ");
+// if(productSell==0){
+//         console.log("Removed Item: "+Products.splice(0,1));
+// } 
+// else if(productSell==1){
+//         console.log("Removed Item: "+Products.splice(1,1));
+// }
+// else if(productSell==2){
+//         console.log("Removed Item: "+Products.splice(2,1));
+// }
+// else if(productSell==3){
+//         console.log("Removed Item: "+Products.splice(3,1));
+// }
+// else 
+// {
+//         console.log("Invalid input");
+// }
+// alert("Remaining Items:  "+Products);
+// alert("Total number of Items Remaining:  "+Products.length);
+
+       //       Task 7
+// let nationality = prompt("Enter your Nationality: ");
+// nationality = nationality.toLowerCase();
+// let gender = prompt("Enter your Gender: ");
+// gender = gender.toLowerCase();
+//  age = +prompt("Enter your Age: ");
+
+// if(nationality == 'pakistani' || nationality == 'indian')
+// {
+//         if(gender == 'male' && age > 18){
+//                 alert("You are Eligible to vote");
+//         }
+//         else if(gender == 'female' && age > 18)
+//         {
+//                 maritalStatus = prompt("Enter your Marital Status: \n Married \n Unmarried");
+//                 maritalStatus - maritalStatus.toLowerCase();
+//                 if(maritalStatus == 'married'){
+//                         alert("You are Eligible to vote");       
+//                 }
+//                 else{
+//                         alert("You are not Eligible to vote");
+//                 }
+//         }
+//         else if(age <= 18){
+//                 alert("You are not Eligible to vote, you are underage");
+//         }
+// }
+// else{
+//         alert("You are not Eligible to vote");
+// }
+
+               //  Task 8
+//  worldcupSquad = ["Babar Azam", "Shadab Khan", "Asif Ali","Fakhar Zaman","Haider Ali","Haris Rauf","Hassan Ali","Imad Waseem","Muhammad Hafeez","Muhammad Nawaz","Muhammad Rizwan","Muhammad Waseem","Sarfaraz Ahmed","Shaheen Afridi","Shoaib Malik","Sohaib Maqsood"];
+//  alert("Player selected for WorldCup \n\n"+WorldCupSquad);
+//  finalTeamPlayers = WorldCupSquad.slice(0,11);
+//  alert("Final Team Players playing tommorow's Match\n\n"+finalTeamPlayers);
+
